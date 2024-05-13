25dcbcb245d081a21386ae04beec28d1569bd9b3
Barbuceanu-Constantin <126708092+Barbuceanu-Constantin@users.noreply.github.com>
2024-05-13 12:34:29 +0300

    Transactions entity
    Transactions entity

--- app/src/main/java/com/barbuceanuconstantin/proiectlicenta/data/Transactions.kt
+++ app/src/main/java/com/barbuceanuconstantin/proiectlicenta/data/Transactions.kt
@@ -1,15 +1,37 @@
 package com.barbuceanuconstantin.proiectlicenta.data
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
 data class Transactions(
-    @PrimaryKey val id: Int,
-    val category: String,
+    @PrimaryKey(autoGenerate = true) val id: Int,
+
+    @ColumnInfo(name = "category_name")
+    val categoryName: String,
+
+    @ColumnInfo(name = "budget_id")
     val budgetId: Int,
+
+    @ColumnInfo(name = "value")
     var value: Double,
+
+    @ColumnInfo(name = "description")
     var description: String,
+
+    @ColumnInfo(name = "date")
     var date: String,
-    var payee: String,
-)
+
+    @ColumnInfo(name = "payee")
+    var payee: String
+) {
+    constructor(categoryName: String,
+                budgetId: Int,
+                value: Double,
+                description: String,
+                date: String,
+                payee: String): this( id = 0, categoryName = categoryName, budgetId = budgetId,
+                                      value = value, description = description, date = date,
+                                      payee = payee)
+}
